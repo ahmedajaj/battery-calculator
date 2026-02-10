94f39c9139998ff909ab23d46d5775edc68c988a
Ahmed Azhazh <ajajahmed96@gmail.com>
2026-02-10 10:28:23 +0200

    fix: set base path for battery-calculator

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/battery-calculator/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
